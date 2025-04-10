79ab775313dad9e8c3279a92e294b4ad1d90de49
Aniruddh <utekaraniruddh6022@gmail.com>
2025-04-10 12:42:16 +0530

    Done with setting up the ground here...

---
+++ 01basicreate/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    
+    <h1>HeHe</h1>
+   
+  );
+}
+
+export default App;
---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+
+  return (
+   <h2>Hello here</h2>
+  )
+}
+
+export default App
